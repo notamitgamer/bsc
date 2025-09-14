ba73502571fe44aa0dccd23f148cbe2109eb4527
Amit Dutta <amitdutta4255@gmail.com>
2025-09-14 20:20:22 +0530

    Add files via upload

---
+++ tution-c/P010.c
@@ -0,0 +1,23 @@
+/* WAP to find and display the difference
+between compound Interest and Simple Interest.
+Take principle amount as input.
+Hint :	si = (p * r * t) / 100
+		a = p * ((1 + (r / 100)) ^ t)
+		ci = a - p
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main() {
+	double p, r, t, si, a, ci, dif;
+	printf("Enter the principle amount, rate of interest, time in year : ");
+	scanf("%lf %lf %lf", &p, &r, &t);
+	si = (p * r * t) / 100;
+	a = p * pow((1 + (r / 100)), t);
+	ci = a - p;
+	dif = ci - si;
+	printf("\nSimple Interest     : %lf"
+		   "\nCompound Interest   : %lf"
+		   "\nInterest Difference : %lf", si, ci, dif);
+	return 0;
+}
---
+++ tution-c/P011.c
@@ -0,0 +1,18 @@
+/* The time period of a simple pendulam is
+given by the formula :
+	t = 2 * pi * square_root(l / g)
+WAP to calculate T take length(L) and gravity
+as input
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main() {
+	double l, g, t;
+	printf("Enter the Length and Gravity measures : ");
+	scanf("%lf %lf", &l, &g);
+	t = 2 * M_PI * sqrt(l / g);
+	// using M_PI variable for PI value from math.h header file
+	printf("\nTime Period : %lf", t);
+	return 0;
+}
