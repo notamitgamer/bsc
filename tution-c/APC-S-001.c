8d5b388a5475e8482c054e4d9baf29b4eacfdbba
Amit Dutta <amitdutta4255@gmail.com>
2025-09-14 20:37:33 +0530

    Add files via upload

---
+++ tution-c/APC-S-001.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int a = 9, b = 4, c;
+	c = a + b;
+	printf("A + B = %d\n", c);
+	c = a / b;
+	printf("A / B = %d\n", c);
+	c = a % b;
+	printf("A %% B = %d\n", c);
+	return 0;
+}
