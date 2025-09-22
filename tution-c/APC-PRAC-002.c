83af356292aa0b840f1d5d3f1cfd5349b9a78cf7
Amit Dutta <amitdutta4255@gmail.com>
2025-09-22 20:40:37 +0530

    Add files via upload

--- tution-c/APC-PRAC-002.c
+++ tution-c/APC-PRAC-002.c
@@ -1,2 +1,15 @@
 /* WAP to calculate area of a circle using math library
 method. Take radius of the circle as input. */
+/* Author - Amit Dutta, Date - 18th SEP, 2025 */
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double r, area;
+    printf("Enter the radius of the circle : ");
+    scanf("%lf", &r);
+    area = M_PI * pow(r, 2);
+    printf("\nArea of the circle : %g", area);
+    return 0;
+}
--- tution-c/APC-PRAC-005.c
+++ tution-c/APC-PRAC-005.c
@@ -1,1 +1,18 @@
-/* WAP a program that accept number of days as input and represent it as years, months and days. */
+/* WAP a program that accept number of days
+as input and represent it as years, months and days. */
+/* Author - Amit Dutta, Date - 19th SEP, 2025 */
+
+#include <stdio.h>
+int main()
+{
+    int days, months, years, temp;
+    printf("Enter the No. of days : ");
+    scanf("%d", &days);
+    temp = days;
+    years = days / 365;
+    days = days % 365;
+    months = days / 30;
+    days = days % 30;
+    printf("%d Days = %d Years, %d Months, %d Days", temp, years, months, days);
+    return 0;
+}
