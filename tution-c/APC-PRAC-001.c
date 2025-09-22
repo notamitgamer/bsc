8dc51e5a4cc76212940855e944c6f0cd920c8550
Amit Dutta <amitdutta4255@gmail.com>
2025-09-22 20:44:55 +0530

    Update APC-PRAC-001.c

--- tution-c/APC-PRAC-001.c
+++ tution-c/APC-PRAC-001.c
@@ -1,14 +1,1 @@
 
-
-#include<stdio.h>
-int main() {
-    double length, breadth, area, perimeter;
-    printf("Enter the length and breadth of the Rectangle : ");
-    scanf("%lf %lf", &length, &breadth);
-    area = length * breadth;
-    perimeter = 2 * (length + breadth);
-    printf("\nArea of the Rectangle : %g"
-           "\nPerimeter of the Rectangle : %g", area, perimeter);
-    return 0;
-
-}
