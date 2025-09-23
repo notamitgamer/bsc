bf030509f168fb8d211ab4f5819b1e4a7889b165
Amit Dutta <amitdutta4255@gmail.com>
2025-09-23 14:08:59 +0530

    Add files via upload

--- tution-c/APC-PRAC-009.c
+++ tution-c/APC-PRAC-009.c
@@ -1,1 +1,14 @@
 /* WAP to swap two integer variable without using Third variable. */
+/* Author - Amit Dutta, Date - 19th SEP, 2025 */
+
+#include <stdio.h>
+int main()
+{
+    int a = 4, b = 6;
+    printf("Before swap : A = %d and B = %d", a, b);
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("\nAfter swap  : A = %d and B = %d", a, b);
+    return 0;
+}
--- tution-c/APC-PRAC-010.c
+++ tution-c/APC-PRAC-010.c
@@ -1,3 +1,17 @@
-/* WAP to calculate and display the valve of the given expression : 
- (1/a^3) + (1/(b+2)^3) + (1/(c^4 + root(2))) 
- take a, b, c as input. */
+/* WAP to calculate and display the valve of the given expression :
+ (1/a^3) + (1/(b+2)^3) + (1/(c^4 + root(2)))
+ take a, b, c as input.
+*/
+/* Author - Amit Dutta, Date - 19th SEP, 2025 */
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double a, b, c, result;
+    printf("Enter the value for a, b and c : ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    result = (1 / pow(a, 3)) + (1 / pow((b + 2), 3)) + (1 / (pow(c, 4) + sqrt(2)));
+    printf("\nResult = %g", result);
+    return 0;
+}
