6e1d9a8518fe45cafd8d352bce801b13cf2dd9b5
Amit Dutta <amitdutta4255@gmail.com>
2025-11-05 21:28:42 +0530

    new -05112025 _KI002

---
+++ khurapati-idea/KI002.c
@@ -0,0 +1,15 @@
+/* Author - Amit Dutta, Date - 05th November, 2025 */
+
+/* Plan - checking for edge case */
+
+#include <stdio.h>
+int main()
+{
+    printf("Starting.....\n");
+
+    for (int i = 1; ; i++)
+    {
+        printf("i = %d\n", i);
+    }
+    return 0;
+}
