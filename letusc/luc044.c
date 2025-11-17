6e3d36f1298b6ac2861f63f02e19fe2ee312461f
Amit Dutta <amitdutta4255@gmail.com>
2025-11-17 21:00:10 +0530

    new -17112025 _letusc/luc44, 45

---
+++ letusc/luc044.c
@@ -0,0 +1,28 @@
+/* Any year is entered through the keyboard. Write a function to
+determine whether the year is aleap year or not. */
+/* Author - Amit Dutta, Date - 17th November, 2025 */
+/* Let Us C, Chap- 8, Page - 144, Qn No.: C(1) */
+
+#include <stdio.h>
+
+int leapYear(int);
+
+int leapYear(int year)
+{
+    if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int year;
+    printf("Enter the year : ");
+    scanf("%d", &year);
+    if (leapYear(year))
+        printf("\nYear %d is a Leap Year.", year);
+    else
+        printf("\nYear %d is not a Leap Year.", year);
+    return 0;
+}
---
+++ letusc/luc045.c
@@ -0,0 +1,63 @@
+/* A position integer is entered through the keyboard. Write a Function
+to obtain the prime factors of this number. 
+For example, prime factors of 24 are 2, 2, 2 and 3, whereas prime 
+factors of 35 are 5 and 7
+*/
+/* Author - Amit Dutta, Date - 17th November, 2025 */
+/* Let Us C, Chap- 8, Page - 144, Qn No.: C(2) */
+
+#include <stdio.h>
+#include <math.h>
+
+void findPrimeFactors(int n)
+{
+    int temp_n = n;
+
+    if (temp_n == 1)
+    {
+        printf("Prime factors of %d are: None.\n", n);
+        return;
+    }
+
+    printf("Prime factors of %d are:", n);
+
+    while (temp_n % 2 == 0)
+    {
+        printf(" %d", 2);
+        temp_n = temp_n / 2;
+    }
+
+    for (int i = 3; i <= (int)sqrt(temp_n); i = i + 2)
+    {
+        while (temp_n % i == 0)
+        {
+            printf(" %d", i);
+            temp_n = temp_n / i;
+        }
+    }
+
+    if (temp_n > 2)
+    {
+        printf(" %d", temp_n);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    printf("Enter a positive integer to get the prime factors: ");
+    if (scanf("%d", &n) != 1)
+    {
+        printf("Error: Invalid input. Please enter an integer.\n");
+        return 1;
+    }
+    if (n <= 0)
+    {
+        printf("Error: Please enter a POSITIVE integer.\n");
+        return 1;
+    }
+    findPrimeFactors(n);
+    return 0;
+}
