348382cc42bb766de8de6d06d954063bbd2b0422
Amit Dutta <amitdutta4255@gmail.com>
2025-11-24 20:22:06 +0530

    new -24112025 .\letusc\luc046, 047

---
+++ letusc/luc046.c
@@ -0,0 +1,34 @@
+/* Given three variables x, y, z, write a function to circularly shift their
+values to right. In other words, if x = 5, y = 8, z = 10, after circular
+shift y = 5, z = 8, x = 10. cal the function with variables a, b, c to
+circularly shift values.
+*/
+/* Author - Amit Dutta, Date - 24th November, 2025 */
+/* Let Us C, Chap- 9, Page - 163, Qn No.: C(a) */
+
+#include <stdio.h>
+
+void circularShift(int *, int *, int *);
+
+int main()
+{
+    int x = 5, y = 8, z = 10;
+
+    printf("--- Before Shift ---\n");
+    printf("x: %d, y: %d, z: %d", x, y, z);
+
+    circularShift(&x, &y, &z);
+
+    printf("\n--- After Shift ---\n");
+    printf("x: %d, y: %d, z: %d", x, y, z);
+
+    return 0;
+}
+
+void circularShift(int *x, int *y, int *z)
+{
+    int temp = *z;
+    *z = *y;
+    *y = *x;
+    *x = temp;
+}
---
+++ letusc/luc047.c
@@ -0,0 +1,31 @@
+/* Define a function that receives weight of a commodity in kilograms
+and returns the equivalent weight in Grams, Tons and pounds. Call
+this fuction from main() and print the results in main()
+*/
+/* Author - Amit Dutta, Date - 24th November, 2025 */
+/* Let Us C, Chap- 9, Page - 163, Qn No.: C(b) */
+
+#include <stdio.h>
+
+void convertWeight(double, double *, double *, double *);
+
+int main()
+{
+    double weightGram, weightPound, weightKG, weightTON;
+    printf("Enter the weight of the comodity in Kilogram(s): ");
+    scanf("%lf", &weightKG);
+
+    convertWeight(weightKG, &weightGram, &weightPound, &weightTON);
+    printf("\n%g Kilogram(s) = %.04f Gram(s)"
+           "\n%g Kilogram(s) = %.04f Pound(s)"
+           "\n%g Kilogram(s) = %.04f TON(s)",
+           weightKG, weightGram, weightKG, weightPound, weightKG, weightTON);
+    return 0;
+}
+
+void convertWeight(double weightKG, double *weightGram, double *weightPound, double *weightTON)
+{
+    *weightGram = weightKG * 1000.0;
+    *weightPound = weightKG * 2.2046226218;
+    *weightTON = weightKG / 1000.0;
+}
