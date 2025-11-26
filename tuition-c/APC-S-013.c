bc10f39bed4d8c00a3c5c2a69727c123f054e6a4
Amit Dutta <amitdutta4255@gmail.com>
2025-11-26 16:35:02 +0530

    new -26112025 .\tuition-c\APC-S-013.c

---
+++ tuition-c/APC-S-013.c
@@ -0,0 +1,27 @@
+/* Write a fuction to calculate the average of an array */
+/* Author: Amit Dutta, Date: 26-11-2025 */
+
+#include <stdio.h>
+
+double average(int []);
+
+int main()
+{
+    int marks[10], i;
+    printf("\nEnter 10 numbers: \n");
+    for (i = 0; i <= 9; i++)
+    {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &marks[i]);
+    }
+    printf("\nAverage = %g", average(marks));
+    return 0;
+}
+
+double average(int marks[10])
+{
+    int sum = 0;
+    for (int i = 0; i <= 9; i++)
+        sum += marks[i];
+    return sum / 10.0;
+}
