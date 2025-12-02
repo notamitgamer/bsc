572fea78023bb0a54e1eb089cec5320fe8b6ceba
Amit Dutta <amitdutta4255@gmail.com>
2025-12-02 20:10:32 +0530

    new -02122025 .\assignment\assignment08, 09

---
+++ assignment/assignment08.c
@@ -0,0 +1,49 @@
+/* Write a C program that includes a user-defined function named countSetBits with the
+signature int countSetBits(int num);. The function should count and return the number of
+set bits (1s) in the binary representation of the given number. */
+
+#include <stdio.h>
+
+int countSetBits(int);
+
+int main()
+{
+    int num, result;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    if (result = countSetBits(num))
+    {
+        printf("\nNumber of set bits in %d: %d", num, result);
+    }
+    else
+    {
+        printf("\nThere is no set bits in %d", num);
+    }
+    return 0;
+}
+
+int countSetBits(int num)
+{
+    int count = 0;
+    if (num < 0)
+    {
+        printf("\nOnly positive numbers are allowed to maintain consistency. \nUsing absolute value %d", -num);
+        num = -num;
+    }
+    while (num > 0)
+    {
+        if (num & 1)
+        {
+            count++;
+        }
+        num >>= 1;
+        /*
+        Another method to do the above bitwise calculation...
+        if(num % 2 == 1) {
+            count++;
+        }
+        num /= 2;
+        */
+    }
+    return count;
+}
---
+++ assignment/assignment09.c
@@ -0,0 +1,24 @@
+/* Write a C program that includes a user-defined function named setBit with the signature
+int setBit(int num, int position);. The function should set the bit at the specified position
+(0-indexed) to 1 and return the modified number. */
+
+#include <stdio.h>
+
+int setBit(int, int);
+
+int main()
+{
+    int num, position;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    printf("Enter the postion where you want to set the bit (0-indexed): ");
+    scanf("%d", &position);
+    printf("\nModified number= %d", setBit(num, position));
+    return 0;
+}
+
+int setBit(int num, int position)
+{
+    int mask = 1 << position;
+    return num | mask;
+}
