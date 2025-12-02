b86f82d44980e4b86afdfd9d9ccee9d1a958d412
Amit Dutta <amitdutta4255@gmail.com>
2025-12-02 11:49:06 +0530

    delete -02122025 .\practice-c\pc012, 013

--- practice-c/pc013.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//just maintaining the streak
-int main() {
-    char name[50];
-    printf("Enter your name: ");
-    fgets(name, sizeof(name), stdin); // Read from standard input (keyboard)
-    printf("Hello, %s", name); // Prints the name with the newline if entered
-    return 0;
-}
