4cda93e7b59ef392aee75984ce6462dc996c9e7e
Amit Dutta <amitdutta4255@gmail.com>
2025-12-22 20:42:19 +0530

    [2025-12-22] : switched to compact license header and stricter usage warnings

--- assignment-secondary/assignment-s-20.c
+++ assignment-secondary/assignment-s-20.c
@@ -1,12 +1,13 @@
 /*
  * ======================================================================================
  * COPYRIGHT (C) 2025 AMIT DUTTA. ALL RIGHTS RESERVED.
- * Repository : https://github.com/notamitgamer/bsc
+ * Repository : https://github.com/notamitgamer/bsc/tree/main/assignment-secondary
  * License    : ESAL-1.0 ( https://aranag.site/license )
  * ======================================================================================
  * [ ACADEMIC INTEGRITY WARNING ]
  * The use of this code for academic assignments at ANY educational institution,
  * college, or university is STRICTLY PROHIBITED.
+ * Any other use requires prior written permission from the author.
  * Violations will be reported as academic misconduct.
  * ======================================================================================
  */
