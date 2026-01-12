0028dfb0532f57aeff05105b6854e4fa0854cd92
Amit Dutta <amitdutta4255@gmail.com>
2026-01-12 19:33:14 +0530

    [2026-01-12] : .\Semester_1\tuition-c : added new tuition file

---
+++ Semester_1/tuition-c/P065.c
@@ -0,0 +1,48 @@
+/*
+ * ======================================================================================
+ * COPYRIGHT (C) 2026 AMIT DUTTA. ALL RIGHTS RESERVED.
+ * Repository : https://github.com/notamitgamer/bsc
+ * License    : ESAL-1.0 ( https://esal.amit.is-a.dev )
+ * ======================================================================================
+ * [ ACADEMIC INTEGRITY WARNING ]
+ * The use of this code for academic assignments at ANY educational institution,
+ * college, or university is STRICTLY PROHIBITED.
+ * Any other use requires prior written permission from the author.
+ * Violations will be reported as academic misconduct.
+ * ======================================================================================
+ */
+
+/* Write a program to reverse a string */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void str_rev(char[]);
+
+int main()
+{
+    char str[101];
+    printf("Enter the string (Max: 100 Character): ");
+    gets(str);
+    printf("\nBefore Reverse: %s", str);
+    str_rev(str);
+    printf("\nAfter Reverse: %s", str);
+    return 0;
+}
+
+void str_rev(char str[])
+{
+    int i, j;
+    char temp;
+    i = 0;
+    j = strlen(str) - 1;
+    while (i < j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+}
