52d48c131701ee0774d9d91c786ed16cadb4e2e2
Amit Dutta <amitdutta4255@gmail.com>
2026-01-26 14:54:48 +0530

    [2026-01-26] : .\Semester_1 : Chnaged License from ESAL-1.0 to MIT

--- Semester_1/practice-c/pc006.c
+++ Semester_1/practice-c/pc006.c
@@ -1,15 +1,9 @@
 /*
- * ======================================================================================
- * COPYRIGHT (C) 2025 AMIT DUTTA. ALL RIGHTS RESERVED.
- * Repository : https://github.com/notamitgamer/bsc
- * License    : ESAL-1.0 ( https://esal.amit.is-a.dev/ )
- * ======================================================================================
- * [ ACADEMIC INTEGRITY WARNING ]
- * The use of this code for academic assignments at ANY educational institution,
- * college, or university is STRICTLY PROHIBITED.
- * Any other use requires prior written permission from the author.
- * Violations will be reported as academic misconduct.
- * ======================================================================================
+ * Author  : Amit Dutta <amitdutta4255@gmail.com>
+ * Date    : 12 Dec 2025
+ * Repo    : https://github.com/notamitgamer/bsc
+ * License : MIT License (See the LICENSE file for details)
+ * Copyright (c) 2025 Amit Dutta
  */
 
 /* Prime number check */
--- Semester_1/tuition-c/P067.c
+++ Semester_1/tuition-c/P067.c
@@ -1,15 +1,7 @@
 /*
- * ======================================================================================
- * COPYRIGHT (C) 2026 AMIT DUTTA. ALL RIGHTS RESERVED.
- * Repository : https://github.com/notamitgamer/bsc
- * License    : ESAL-1.0 ( https://esal.amit.is-a.dev )
- * ======================================================================================
- * [ ACADEMIC INTEGRITY WARNING ]
- * The use of this code for academic assignments at ANY educational institution,
- * college, or university is STRICTLY PROHIBITED.
- * Any other use requires prior written permission from the author.
- * Violations will be reported as academic misconduct.
- * ======================================================================================
+ * Author: Amit Dutta (amitdutta4255@gmail.com) | Date: 14 Jan 2026
+ * Repo: https://github.com/notamitgamer/bsc
+ * License: MIT | Copyright (c) 2026 Amit Dutta
  */
 
 /* Write a program using function which concatenates two string s1 and s2 in to a third string */
