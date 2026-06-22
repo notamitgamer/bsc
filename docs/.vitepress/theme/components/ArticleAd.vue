aac4f460c4cfb60cf7ecd8bec946636767d055a9
Amit Dutta <amitdutta4255@gmail.com>
2026-06-22 17:58:05 +0530

    forced pushed to the end

--- docs/.vitepress/theme/components/ArticleAd.vue
+++ docs/.vitepress/theme/components/ArticleAd.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="article-ad-container">
     <span class="ad-label">ADVERTISEMENT</span>
-    <div class="ad-content">
-      <!-- Your A-Ads Iframe -->
+    
+    <!-- EXACT A-ADS ADAPTIVE UNIT -->
+    <div id="frame" style="width: 100%; margin: auto; position: relative; z-index: 99998;">
       <iframe 
         data-aa='2445210' 
-        src='//ad.a-ads.com/2445210/?size=125x125'
-        style='border:0; padding:0; width:125px; height:125px; overflow:hidden; display:block; margin: 0 auto;' 
+        src='//acceptable.a-ads.com/2445210/?size=Adaptive'
+        style='border:0; padding:0; width:70%; height:90px; overflow:hidden; display: block; margin: auto'
         allowtransparency='true'>
       </iframe>
     </div>
+    
   </div>
 </template>
 
@@ -32,6 +34,6 @@
   font-weight: 700;
   color: var(--vp-c-text-3);
   letter-spacing: 0.5px;
-  margin-bottom: 8px;
+  margin-bottom: 12px;
 }
 </style>
